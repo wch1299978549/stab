ad80f8d6c0e52690f6b130e7f5d0bd156af4e6e3
stab <stab123@163.com>
2019-12-23 17:51:20 +0800

    修改bug

--- src/com/hp/test/Test.java
+++ src/com/hp/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("硬崩钢");
         System.out.println("饮魔刀");
-        System.out.println("刀");
+        System.out.println("刀把");
         System.out.println("尹磨刀");
     }
 }
